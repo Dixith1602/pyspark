85d51c17a13652e196981441019e225c930bfa24
Dixith G R <60172680+Dixith1602@users.noreply.github.com>
2026-01-06 15:03:23 +0530

    Update pyspark.py

--- pyspark.py
+++ pyspark.py
@@ -1,1 +1,11 @@
+# imp
+# No of times Charecter repeated in the list 
 
+list1 = ["Hello students", "Hello pyspark", "Hello rdd"]
+input_rdd = sc.parallelize(list1)
+flat_map_rdd = input_rdd.flatMap(lambda x: x.split(" "))
+
+result = input_rdd.flatMap(lambda x: x.split(" "))\
+                  .map(lambda x: (x, 1))\
+                  .reduceByKey(lambda x, y: x + y)
+print(result.collect())
